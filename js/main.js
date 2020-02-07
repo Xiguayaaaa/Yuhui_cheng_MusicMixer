1198cbb4d79f2259120eb462b641657ee2652d3d
Yuhui Cheng <46694948+Xiguayaaaa@users.noreply.github.com>
2020-02-07 00:13:35 -0500

    adding files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(()=>{
+	console.log("Trevor");
+
+
+
+	const zodiacsigns = document.querySelector(".zodiacsigns"),
+		  resetButton = document.querySelector("#reset"),
+		  popupButton = document.querySelector("#popup"),
+		  closeButton = document.querySelector(".closebox");
+      
